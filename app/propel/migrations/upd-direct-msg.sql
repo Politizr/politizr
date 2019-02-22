d2723ab55492ec7b8696b844dda721abf338ea0f
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2019-02-22 09:05:02 +0100

    Upd SQL

--- app/propel/migrations/upd-direct-msg.sql
+++ app/propel/migrations/upd-direct-msg.sql
@@ -6,7 +6,7 @@ WHERE online = 0
 AND roles LIKE '%ROLE_PROFILE_COMPLETED%'
 AND (banned = 0 OR banned is null);
 
-# UPD DEBATE TO SET OP > @todo > only where published = false
+# UPD DEBATE TO SET OP
 UPDATE `p_d_debate`
 SET `p_e_operation_id`=28,
 `online`=1,
@@ -14,14 +14,17 @@ SET `p_e_operation_id`=28,
 `updated_at`= NOW(),
 `p_l_country_id` = 1,
 `description` = CONCAT('<p>', description, '</p>')
-WHERE p_d_debate.title = 'MESSAGE DIRECT';
+WHERE p_d_debate.title = 'MESSAGE DIRECT'
+AND p_d_debate.created_at > '2019-02-03 16:30:00';
 
 # PLUG TAG 1126 > @todo > only where published = false
 DELETE `p_d_d_tagged_t`
 FROM `p_d_d_tagged_t`
 INNER JOIN `p_d_debate` ON p_d_d_tagged_t.p_d_debate_id = p_d_debate.id
-WHERE p_d_debate.title = 'MESSAGE DIRECT';
+WHERE p_d_debate.title = 'MESSAGE DIRECT'
+AND p_d_debate.created_at > '2019-02-03 16:30:00';
 
 INSERT INTO `p_d_d_tagged_t` (p_tag_id, p_d_debate_id)
 SELECT 1126, id FROM `p_d_debate`
-WHERE p_d_debate.title = 'MESSAGE DIRECT';
+WHERE p_d_debate.title = 'MESSAGE DIRECT'
+AND p_d_debate.created_at > '2019-02-03 16:30:00';
