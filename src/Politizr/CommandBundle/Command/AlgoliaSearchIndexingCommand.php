02ef72837e7578363ea28e52e2d461813ce9525c
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2019-03-02 14:54:10 +0100

    Fix dead code

--- src/Politizr/CommandBundle/Command/AlgoliaSearchIndexingCommand.php
+++ src/Politizr/CommandBundle/Command/AlgoliaSearchIndexingCommand.php
@@ -667,12 +667,4 @@ class AlgoliaSearchIndexingCommand extends ContainerAwareCommand
 
         return $indexedObjects;
     }
-
-    /**
-     *
-     */
-    function encodeURIComponent($str) {
-        $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
-        return strtr(rawurlencode($str), $revert);
-    }
 }
