d1ef957f6d724701b8bbc6e2775c0a10717a0a90
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2019-03-15 08:53:50 +0100

    upd sql

--- app/propel/migrations/upd-localization-grand-debat.sql
+++ app/propel/migrations/upd-localization-grand-debat.sql
@@ -4,7 +4,7 @@ SET p_l_country_id = 1,
  p_l_city_id = NULL,
  p_l_department_id = NULL,
  p_l_region_id = NULL
-WHERE p_c_topic_id IN (16, 17, 18, 19, 20, 21, 22, 23, 24);
+WHERE p_c_topic_id IN (16, 17, 18, 19, 20, 21, 22, 23, 24, 25);
 
 # UPD REACTION TO FRANCE
 UPDATE `p_d_reaction`
@@ -12,4 +12,4 @@ SET p_l_country_id = 1,
  p_l_city_id = NULL,
  p_l_department_id = NULL,
  p_l_region_id = NULL
-WHERE p_c_topic_id IN (16, 17, 18, 19, 20, 21, 22, 23, 24);
+WHERE p_c_topic_id IN (16, 17, 18, 19, 20, 21, 22, 23, 24, 25);
