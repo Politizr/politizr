877fa23380eebc326f97ed0384139456a54225d2
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2019-07-17 17:33:47 +0200

    Remove GTA conversion tracking, use GTA directly

--- src/Politizr/FrontBundle/Resources/public/js/politizr/pages/landingPage.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/pages/landingPage.js
@@ -10,7 +10,6 @@ $(function(){
 // form contact
 $("body").on("click", "button[action='submitDirectMessage']", function(e) {
     // console.log('click submitDirectMessage');
-    gtag_report_conversion();
 
     var form = $(this).closest('form');
     var localLoader = $(this).closest('.formBlock').find('.ajaxLoader').first();
