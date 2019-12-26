ed4112fbddd14b94fe78b76b8d71d656d4e0668e
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2019-12-26 10:59:39 +0100

    M2020 updates

--- src/Politizr/FrontBundle/Resources/public/js/politizr/pages/landingPage.js
+++ src/Politizr/FrontBundle/Resources/public/js/politizr/pages/landingPage.js
@@ -11,6 +11,12 @@ $(function(){
 $("body").on("click", "button[action='submitDirectMessage']", function(e) {
     // console.log('click submitDirectMessage');
 
+    // GTM
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event': 'directMessage'
+    });
+
     var form = $(this).closest('form');
     var localLoader = $(this).closest('.formBlock').find('.ajaxLoader').first();
 
