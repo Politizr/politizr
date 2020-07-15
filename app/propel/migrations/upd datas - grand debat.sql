79f7310cb7d9ceabc9bfc485b5ce49dfc2357c72
Lionel Bouzonville <lionel.bouzonville@gmail.com>
2020-07-15 09:23:59 +0200

    Upd fixtures

--- app/propel/migrations/upd datas - grand debat.sql
+++ app/propel/migrations/upd datas - grand debat.sql
@@ -1,16 +1,16 @@
 # Insertion dans p_c_group_l_c de toutes les villes de France
 INSERT INTO `p_c_group_l_c` (`p_circle_id`, `p_l_city_id`, `created_at`, `updated_at`)
-SELECT 7, `id`, NOW(), NOW() FROM `p_l_city`;
+SELECT 9, `id`, NOW(), NOW() FROM `p_l_city`;
 
 # Inscription de tous les utilisateurs existants
 INSERT INTO `p_u_in_p_c` (`p_circle_id`, `p_user_id`, `created_at`, `updated_at`)
-SELECT 7, `id`, NOW(), NOW()
+SELECT 9, `id`, NOW(), NOW()
 FROM `p_user`;
 
 # MAJ des droits
-UPDATE `p_user` SET `roles` = REPLACE(`roles`, ' ROLE_CIRCLE_7 |', '');
+UPDATE `p_user` SET `roles` = REPLACE(`roles`, ' ROLE_CIRCLE_9 |', '');
 
-UPDATE `p_user` SET `roles` = CONCAT(`roles`, ' ROLE_CIRCLE_7 |'), `updated_at` = NOW();
+UPDATE `p_user` SET `roles` = CONCAT(`roles`, ' ROLE_CIRCLE_9 |'), `updated_at` = NOW();
 
 # Mise en ligne du group et de ses thématiques
 UPDATE `p_circle` SET `online` = 1 WHERE `id` = 7;
